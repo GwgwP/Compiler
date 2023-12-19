50aa356b03c362b51c14c55d087630914918e851
Georgia Petsa <gwgwpet2001@gmail.com>
2023-12-19 18:42:26 +0200

    g

--- example1.py
+++ example1.py
@@ -1,20 +1,8 @@
-# def x(a=1):
-#     x = 3+2+6
-
-# def x(a=1, b=6,c):
-#     x = 7    
-#     for j in v:
-#         print (f)
-
 
 def f(y, x=1):
     print("1")
-    print (y)
 
 
-def f(x, y):
+def f(x, y,t):
     print("2")
-    print(y+2)
 
-
-f(11, 1)
--- myvisitor.java
+++ myvisitor.java
@@ -10,7 +10,7 @@ public class myvisitor extends DepthFirstAdapter
 	private Hashtable<String, Node> functions;
 	private Hashtable<Node, VARIABLE_TYPES> variableTypes;
 	
-	private List<Function> functionList;
+	private List<Function> functionL;
 	/**
 	* All the recognisable potential error types listed
 	*/
@@ -22,7 +22,8 @@ public class myvisitor extends DepthFirstAdapter
 		NONE,                // #5th error
 		MISUSED_FUNCTION,    // #6th error
 		REDEFINED_FUNCTION,  // #7th error
-		UNDEFINED_ARRAY
+		UNDEFINED_ARRAY,
+		REDIFINED_VARIABLE
 		// UNORDERED_PARAMS,
 		// ADD_TYPE_MISSMATCH,
 		// MINUS_TYPE_MISSMATCH,
@@ -45,7 +46,7 @@ public class myvisitor extends DepthFirstAdapter
 		this.variables = variables;
 		this.functions = functions;
 		this.variableTypes = variableTypes;
-		this.functionList = new ArrayList<>();
+		this.functionL = new ArrayList<>();
 	}
 
 
@@ -67,18 +68,78 @@ public class myvisitor extends DepthFirstAdapter
 			AForStatementStatement forLoop = (AForStatementStatement) parent;
 
 			// Check that the second identifer is an existing variable
-			AIdId id2 = (AIdId) forLoop.getRid();
-			if (name.equals(id2.getIdent().getText()) && !variables.containsKey(id2)) { 
-				// Print error message
+			AIdId id2 = (AIdId) forLoop.getRid(); // for x in x is ilegal because x is global var
+			if (name.equals(id2.getIdent().getText()) ) { 
+				printError(node, ERRORS.REDIFINED_VARIABLE);
+			}
+			else if(!variables.containsKey(id2.getIdent().getText())){
 				printError(node, ERRORS.UNDECLARED_VARIABLE);
 			}
-			else{
-				variables.put(id2.getIdent().getText(), id2);
+			
+			String variableType = variableTypes.get(id2).name();
+			if(variables.contains(name))
+			{
+				if (variableType.equals("INTEGER")) 
+				{
+					if(!variableTypes.get(name).name().equals("INTEGER"))
+					{
+						printError(node, ERRORS.TYPE_MISSMATCH);
+					}
+				}
+				else if (variableType.equals("DOUBLE"))
+				{
+					if(!variableTypes.get(name).name().equals("DOUBLE"))
+					{
+						printError(node, ERRORS.TYPE_MISSMATCH);
+					}
+				} 
+				else if (variableType.equals("STRING")) 
+				{
+					if(!variableTypes.get(name).name().equals("STRING"))
+					{
+						printError(node, ERRORS.TYPE_MISSMATCH);
+					}
+				} else if (variableType.equals("NONE")) 
+				{
+					if(!variableTypes.get(name).name().equals("NONE"))
+					{
+						printError(node, ERRORS.TYPE_MISSMATCH);
+					}
+				} 
+				else if (variableType.equals("UNKNOWN")) 
+				{
+					if(!variableTypes.get(name).name().equals("UNKNOWN"))
+					{
+						printError(node, ERRORS.TYPE_MISSMATCH);
+					}
+				}
 			}
+			else
+			{
+				if (variableType.equals("INTEGER")) 
+				{
+					variableTypes.put(node, VARIABLE_TYPES.INTEGER);
+				}
+				else if (variableType.equals("DOUBLE"))
+				{
+					variableTypes.put(node, VARIABLE_TYPES.DOUBLE);
+				} 
+				else if (variableType.equals("STRING")) 
+				{
+					variableTypes.put(node, VARIABLE_TYPES.STRING);
+				} else if (variableType.equals("NONE")) 
+				{
+					variableTypes.put(node, VARIABLE_TYPES.NONE);
+				} 
+				else if (variableType.equals("UNKNOWN")) 
+				{
+					variableTypes.put(node, VARIABLE_TYPES.UNKNOWN);
+				}
+			} 
+			
 		}
 		
-	}
-		
+	} 
 	public void outAIdId(AIdId node)
 	{
 		Node parent = node.parent();
@@ -103,155 +164,183 @@ public class myvisitor extends DepthFirstAdapter
 			// for x in y: and y=[1,2,3] x should be integer, if y =[1.2, 1.3] y should be double etc
 			
 			String variableType = variableTypes.get(id2).name();
-			if (variableType.equals("INTEGER")) 
-			{
-				variableTypes.put(id1, VARIABLE_TYPES.INTEGER);
-			}
-			else if (variableType.equals("DOUBLE"))
-			{
-				variableTypes.put(id1, VARIABLE_TYPES.DOUBLE);
-			} 
-			else if (variableType.equals("STRING")) 
-			{
-				variableTypes.put(id1, VARIABLE_TYPES.STRING);
-			} else if (variableType.equals("NONE")) 
-			{
-				variableTypes.put(id1, VARIABLE_TYPES.NONE);
-			} 
-			else if (variableType.equals("UNKNOWN")) 
-			{
-				variableTypes.put(id1, VARIABLE_TYPES.UNKNOWN);
-			}
-			variables.put(name, node); 
-		}
-		
+			variables.put(id1.getIdent().getText(), node);
+		}	
 	}
 
-	private void printError(AIdId node, myvisitor.ERRORS undeclaredVariable) {
-	}
+	private void printError(Node node, myvisitor.ERRORS error) {
+		switch (error) {
+            case UNDECLARED_VARIABLE:
+                System.out.println("Error at Node " + node + ": Undeclared variable");
+                break;
+            case UNDEFINED_FUNCTION:
+                System.out.println("Error at Node " + node + ": Undefined function");
+                break;
+            case WRONG_FUNCTION_PARAMETERS:
+                System.out.println("Error at Node " + node + ": Wrong function parameters");
+                break;
+            case TYPE_MISSMATCH:
+                System.out.println("Error at Node " + node + ": Type mismatch");
+                break;
+            case NONE:
+                System.out.println("No error at Node " + node);
+                break;
+            case MISUSED_FUNCTION:
+                System.out.println("Error at Node " + node + ": Misused function");
+                break;
+            case REDEFINED_FUNCTION:
+                System.out.println("Error at Node " + node + ": Redefined function");
+                break;
+            case UNDEFINED_ARRAY:
+                System.out.println("Error at Node " + node + ": Undefined array");
+                break;
+            // Add cases for other error types as needed
 
+            default:
+                System.out.println("Unknown error at Node " + node);
+        }
+		System.exit(-1);
+
+	}
+	
 
 	/**
 	 * given example modified to suit our grammar
 	 */
 	@Override
 	public void inADefFuncFunction(ADefFuncFunction node)
-	{
+	{ 
+		boolean flag = false;
 		String fName = node.getId().toString();
-		
-	
-		if (functions.containsKey(fName))
+
+		if (functions.containsKey(fName)) //if function exists check that number of parameters is different
 		{
+			
 			Hashtable<String, Node> functions2 = new Hashtable();
 			functions2 = (Hashtable<String, Node>) functions.clone();
 			
-			int stop = functions2.size();
-			for (int i=0; i<= stop; i++)
+			
+			for (int i=0; i< functions2.size(); i++)
 			{
+				System.out.println("size1:" + functions2.size());
+				functions2.remove(fName);
+				System.out.println("size2:" + functions2.size());
+
+
 				if(functions2.get(fName)!= null) // if functions2.get(i) == fname
 				{
 					ADefFuncFunction func_more = (ADefFuncFunction) functions2.get(fName);
-					
-					if(node.getArgument().size() == func_more.getArgument().size() && node.getArgument().size() == 0) //e.g def f() and def f() is Elegal
-					{
-						
-						printError(node, ERRORS.REDEFINED_FUNCTION);
-					}
-					else
-					{
-						// calculation of same name function's arguments and assignements
-						int count_total_vars = 0;
-						int count_default_vars = 0;
-						AArgArgument argument = ((AArgArgument) (func_more.getArgument().get(0)));
-						if (argument.getAssignValue()!=null) //TODO: CHECK IF IT WORKS
-						{
-							count_total_vars++;
-							count_default_vars++;
+				
+					// calculation of same name function's arguments and assignements
+					int count_total_vars = 0;
+					int count_default_vars = 0;
 
+					int count_total_vars_new = 0;
+					int count_default_vars_new = 0;
+
+					if (func_more.getArgument() == null)//there are no arguments
+					{
+						count_total_vars = 0;
+
+						if(node.getArgument() == null)
+						{
+							count_total_vars_new = 0;
 						}
-						else
+						
+					}
+					else //(func_more.getArgument().get(0)!=null) //there are arguments
+					{
+					
+						AArgArgument argument = ((AArgArgument) (func_more.getArgument().get(0)));
+						
+						if (argument.getId()!=null) //TODO: CHECK IF IT WORKS
 						{
 							count_total_vars++;
+							if(argument.getAssignValue()!=null)
+							{
+								count_default_vars++;
+							}																																				
 						}
 						LinkedList ciav = argument.getCiav();
-						if(ciav!=null)
+						for ( int k = 0; k < ciav.size();k++)
 						{
-							for ( int k = 0; k < ciav.size();k++)
+							ACommaIdCiav c = (ACommaIdCiav) ciav.get(k);
+							if (c.getAssignValue() == null) //TODO: CHECK IF IT WORKS
 							{
-								ACommaIdCiav c = (ACommaIdCiav) ciav.get(k);
-								if (c.getAssignValue()!=null) //TODO: CHECK IF IT WORKS
-								{
-									count_total_vars++;
-									count_default_vars++;
+								count_total_vars++;
 
-								}
-								else
-								{
-									count_total_vars++;
-								}
+							}
+							else
+							{
+								count_default_vars++;
+								count_total_vars++;
 							}
 						}
-						//
-						// calculation of initial name function's arguments and assignements
-						int count_total_vars_new = 0;
-						int count_default_vars_new = 0;
+						
+					}
+					//
+					// calculation of initial name function's arguments and assignements
+					if(node.getArgument()== null) //there are no arguments
+					{
+						count_default_vars_new = 0;
+					}
+					else //(node.getArgument().get(0)!=null) //there are arguments
+					{
 						AArgArgument argument_n = ((AArgArgument) (node.getArgument().get(0)));
-						if (argument.getAssignValue()!=null) //TODO: CHECK IF IT WORKS
+						if (argument_n.getId()!=null) //TODO: CHECK IF IT WORKS
 						{
 							count_total_vars_new++;
-							count_default_vars_new++;
+							if(argument_n.getAssignValue()!=null)
+							{
 
-						}
-						else
-						{
-							count_total_vars++;
+								count_default_vars_new++;
+							}																																				
 						}
 						LinkedList ciav_n = argument_n.getCiav();
-						if(ciav!=null)
+						for ( int k = 0; k < ciav_n.size();k++)
 						{
-							for ( int k = 0; k < ciav_n.size();k++)
+							ACommaIdCiav c_n = (ACommaIdCiav) ciav_n.get(k);
+							if (c_n.getAssignValue()==null) //TODO: CHECK IF IT WORKS
 							{
-								ACommaIdCiav c = (ACommaIdCiav) ciav_n.get(k);
-								if (c.getAssignValue()!=null) //TODO: CHECK IF IT WORKS
-								{
-									count_total_vars_new++;
-									count_default_vars_new++;
+								count_total_vars_new++;
 
-								}
-								else
-								{
-									count_total_vars_new++;
-								}
+							}
+							else
+							{
+								count_default_vars_new++;
+								count_total_vars_new++;
 							}
 						}
 						
-						if(count_total_vars - count_default_vars == count_total_vars_new - count_default_vars_new)
-						{
-							printError(node, nul);
-						}
-						
-
 					}
-					functions2.remove(i);
-				}				
+					
+					if(count_total_vars == count_total_vars_new && !flag ||count_total_vars - count_default_vars == count_total_vars_new - count_default_vars_new && !flag)
+					{
+						
+						System.out.println("total vars:"+count_total_vars);
+						System.out.println("def vars:"+count_default_vars);
+						System.out.println("total vars new :"+count_total_vars_new);
+						System.out.println("def vars new:"+count_default_vars_new);
+
+
+						printError(node, ERRORS.REDEFINED_FUNCTION);
+						flag = true;
+					}
+					
+				}		
 				
-				printError(null, ERRORS.DEFINED_FUNCTION);
-				//System.out.println("Line " + 1 + ": " +" Function " + fName +" is already defined");
-				
+
 			}
 		}
-		else
-		{
-			functions.put(fName, node);
-		}
+		
 	}
-	private void printError(ADefFuncFunction node, myvisitor.ERRORS redefinedFunction) {
-	}
+	
 
 
 	public void outADefFuncFunction(ADefFuncFunction node)
 	{
-
+		String fName = node.getId().toString();
+		functions.put(fName, node);
 	}
 	@Override
 	public void inAIdentifierExpression(AIdentifierExpression node)
